f25e45d8bfbacea506c465dca22a17ea1803b655
dagolinuxoid <daniel1kdago@gmail.com>
2017-09-11 21:39:51 +0300

    add list of heroes

--- fessapp.py
+++ fessapp.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
+heroes = [
+    {'name':'juggernaut', 'description':'agility'},
+    {'name':'mortred', 'description':'agility'},
+    {'name':'skeleton king', 'description': 'strength'},
+    {'name':'maigna', 'description':'agility'},
+    {'name':'faceless void', 'description':'agility'}
+]
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index(heroes):
+    return render_template('index.html', heroes=heores)
